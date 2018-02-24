6e4414e11eec2422f91472141949382f479128ad
Miguel <Miguel@DESKTOP-KMDHUDG>
2018-02-24 18:17:37 +0100

    Part A finished

--- A/Reference.java
+++ A/Reference.java
@@ -1,8 +1,18 @@
 package concurrent_assignment1.A;
 
 /**
- * Get the reference of the main thread */
-
+ * Get the reference of the main thread
+ */
 public class Reference {
 
+    public static void main(String[] args) throws InterruptedException {
+        Thread h = Thread.currentThread();
+        h.setName("A");
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i + "" + h.getName());
+                Thread.sleep(2000);
+            }
+        }
+    }
 }
