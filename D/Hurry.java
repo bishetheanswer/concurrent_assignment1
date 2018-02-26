fa6d4c87fb11aac777c6c904993ba028b8f58718
usuario <usuario@161.67.196.135>
2018-02-26 10:14:09 +0100

    Part D finished

--- D/Hurry.java
+++ D/Hurry.java
@@ -1,17 +1,38 @@
 package concurrent_assignment1.D;
+
 /**
- * Hurry must:
- * - Be the main thread
- * - Create the Lazy thread.
- * - Wait for Lazy to finish. Each second it must print the message: “Aren’t you ready yet?” and checks if Lazy is finished.
- * - If Lazy isn’t finished after 5 seconds, Hurry will claim “You are resting in your laurels… and I am leaving!” and interrupts Lazy. Then it waits for Lazy to finish.
- * - However, if Lazy finishes before the 5 seconds deadline, Hurry says “At last, a turtle runs rings round you!”.
- * 
+ * Hurry must: - Be the main thread - Create the Lazy thread. - Wait for Lazy to
+ * finish. Each second it must print the message: “Aren’t you ready yet?” and
+ * checks if Lazy is finished. - If Lazy isn’t finished after 5 seconds, Hurry
+ * will claim “You are resting in your laurels… and I am leaving!” and
+ * interrupts Lazy. Then it waits for Lazy to finish. - However, if Lazy
+ * finishes before the 5 seconds deadline, Hurry says “At last, a turtle runs
+ * rings round you!”.
+ *
  * @author yournamehere
  *
  */
-
-
 public class Hurry {
 
+    public static void main(String[] args) throws InterruptedException {
+        Lazy l = new Lazy();
+        l.start();
+        int j = 0;
+        for (int i = 0; i < 5; i++) {
+            if (!l.isAlive()) {
+                System.out.println("At last, a turtle runs rings round you!");
+                j = 2;
+                break;
+            }
+
+            System.out.println("Aren’t you ready yet?");
+            Thread.sleep(1000);
+        }
+        if (j != 2) {
+            System.out.println("You are resting in your laurels… and I am leaving!");
+        }
+        l.interrupt();
+        l.join();
+        System.out.println("THE END");
+    }
 }
--- D/Lazy.java
+++ D/Lazy.java
@@ -1,17 +1,59 @@
 package concurrent_assignment1.D;
 
-/** Lazy must:
- * - Extend Thread
- * - Enter in a loop which iterates a random number of times between 2 to 8.
- * - Sleep 1 second
- * - At each iteration, it displays a random message among: “I am dressing up…”, “Just a sec, please…”, or “These clothes do not suit me…”.
- * - If Hurry interrupts Lazy before it is finished, Lazy claims: “That’s not cricket, please play the game!”.
- * - If Lazy finishes before being interrupted, it says “I am ready, the early bird catches the worm!”.
- * 
+import java.util.Random;
+
+/**
+ * Lazy must: - Extend Thread - Enter in a loop which iterates a random number
+ * of times between 2 to 8. - Sleep 1 second - At each iteration, it displays a
+ * random message among: “I am dressing up…”, “Just a sec, please…”, or “These
+ * clothes do not suit me…”. - If Hurry interrupts Lazy before it is finished,
+ * Lazy claims: “That’s not cricket, please play the game!”. - If Lazy finishes
+ * before being interrupted, it says “I am ready, the early bird catches the
+ * worm!”.
+ *
  * @author yournamehere
  *
  */
+public class Lazy extends Thread {
 
-public class Lazy {
+    Random ran = new Random();
+    int n;
+    int j;
+    int x = 0;
+
+    public void run() {
+        n = ran.nextInt(7 + 2);
+
+        for (int i = 0; i < n && !Thread.interrupted(); i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (Exception E) {
+                interrupt();
+            }
+            if (!Thread.interrupted()) {
+
+                j = ran.nextInt(3);
+                switch (j) {
+                    case 0:
+                        System.out.println("I am dressing up…");
+                        break;
+                    case 1:
+                        System.out.println("Just a sec, please…");
+                        break;
+                    case 2:
+                        System.out.println("These clothes do not suit me…");
+                        break;
+
+                }
+            } else {
+                System.out.println("That’s not cricket, please play the game!");
+                x = 2;
+            }
+        }
+        if (x != 2) {
+            System.out.println("I am ready, the early bird catches the worm!");
+        }
+
+    }
 
 }
